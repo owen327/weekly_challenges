44818615d61ed023aafa7ba4966086bfda2608a5
Owen Tran <owen_327@hotmail.com>
2016-01-30 09:36:45 +0000

    Refactor methods

--- grade_school/grade_school.rb
+++ grade_school/grade_school.rb
@@ -4,16 +4,14 @@ class School
   end
 
   def to_h
-    @school.keys.sort.each_with_object({}) do |key, new_array|
-      new_array[key] = grade(key)
-    end
+    @school.sort.to_h
   end
 
   def add(name, grade)
-    @school[grade].nil? ? @school[grade] = [name] : @school[grade] << name
+    @school[grade].nil? ? @school[grade] = [name] : @school[grade] = @school[grade].push(name).sort
   end
 
   def grade(specific_grade)
-    @school.fetch(specific_grade, []).sort
+    @school.fetch(specific_grade, [])
   end
 end
