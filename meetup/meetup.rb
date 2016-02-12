0775233f2fa9fb271d1d77a160edf8fb071fab47
Owen Tran <owen_327@hotmail.com>
2016-02-12 03:01:34 +0000

    Refactor methods

--- meetup/meetup.rb
+++ meetup/meetup.rb
@@ -1,17 +1,13 @@
 class Meetup
+  INDEX = { first: 0, second: 1, third: 2, fourth: 3, last: -1 }
+
   def initialize(month, year)
-    @month_days = Date.new(year, month)...Date.new(year, month) >> 1
+    @range = Date.new(year, month)...Date.new(year, month) >> 1
   end
 
   def day(weekday, schedule)
-    return days_with(weekday)[{ first: 0, second: 1, third: 2, fourth: 3,
-                                last: -1 }[schedule]] if schedule != :teenth
-    days_with(weekday).select { |dy| (13..19).include?(dy.mday) }[0]
-  end
-
-  private
-
-  def days_with(weekday)
-    @month_days.select { |dy| dy.strftime('%A').downcase.to_sym == weekday }
+    days = @range.select { |dy| dy.strftime('%A').downcase.to_sym == weekday }
+    return days[INDEX[schedule]] if schedule != :teenth
+    days.select { |dy| (13..19).include? dy.mday }[0]
   end
 end
