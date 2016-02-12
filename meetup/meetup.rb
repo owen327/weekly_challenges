729696892860b9d1d8cdf8b600844309b31cc686
Owen Tran <owen_327@hotmail.com>
2016-02-12 16:54:17 +0000

    Remove contant

--- meetup/meetup.rb
+++ meetup/meetup.rb
@@ -1,12 +1,11 @@
 class Meetup
-  INDEX = { first: 0, second: 1, third: 2, fourth: 3, last: -1 }
-
   def initialize(month, year)
     @range = Date.new(year, month)...Date.new(year, month) >> 1
   end
 
   def day(weekday, schedule)
     days = @range.select { |dy| dy.strftime('%A').downcase.to_sym == weekday }
-    days[INDEX.fetch(schedule, days.index { |dy| (13..19).include? dy.mday })]
+    days[{ first: 0, second: 1, third: 2, fourth: 3, last: -1,
+           teenth: (days.index { |dy| (13..19).include? dy.mday }) }[schedule]]
   end
 end
