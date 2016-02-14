1b4d8d10641eb81ccaf163e2aa9b5b75c4d04512
Owen Tran <owen_327@hotmail.com>
2016-02-14 18:04:12 +0000

    Refactor after live session

--- meetup/meetup.rb
+++ meetup/meetup.rb
@@ -1,11 +1,13 @@
 class Meetup
+  MARKER = { first: 1, second: 8, third: 15, fourth: 22, last: -7, teenth: 13 }
+
   def initialize(month, year)
-    @range = Date.new(year, month)...Date.new(year, month) >> 1
+    @week = MARKER.map do |sch, num|
+      [sch, Date.new(year, month, num)..Date.new(year, month, num + 6)]
+    end.to_h
   end
 
   def day(weekday, schedule)
-    days = @range.select { |dy| dy.strftime('%A').downcase.to_sym == weekday }
-    days[{ first: 0, second: 1, third: 2, fourth: 3, last: -1,
-           teenth: (days.index { |dy| (13..19).include? dy.mday }) }[schedule]]
+    @week[schedule].find { |dy| dy.strftime('%A').downcase.to_sym == weekday }
   end
 end
