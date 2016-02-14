0380a75a3184e775549b342a4f0ef1438d95ea1f
Owen Tran <owen_327@hotmail.com>
2016-02-14 20:20:53 +0000

    Rename instance variable

--- meetup/meetup.rb
+++ meetup/meetup.rb
@@ -2,12 +2,12 @@ class Meetup
   MARKER = { first: 1, second: 8, third: 15, fourth: 22, last: -7, teenth: 13 }
 
   def initialize(month, year)
-    @week = MARKER.map do |sch, num|
+    @range = MARKER.map do |sch, num|
       [sch, Date.new(year, month, num)..Date.new(year, month, num + 6)]
     end.to_h
   end
 
   def day(weekday, schedule)
-    @week[schedule].find { |dy| dy.strftime('%A').downcase.to_sym == weekday }
+    @range[schedule].find { |dy| dy.strftime('%A').downcase.to_sym == weekday }
   end
 end
