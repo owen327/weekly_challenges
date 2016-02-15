b5b1ffe2b658a176e80e5a3979de2414617ff5a0
Owen Tran <owen_327@hotmail.com>
2016-02-15 01:13:54 +0000

    Move @ref into initialize

--- meetup/meetup.rb
+++ meetup/meetup.rb
@@ -1,13 +1,13 @@
 class Meetup
   def initialize(month, year)
     @range = (Date.new(year, month)...Date.new(year, month) >> 1).to_a
+    @ref = { first: 0, second: 7, third: 14, fourth: 21, last: -7, teenth: 12 }
   end
 
   def day(weekday, schedule)
-    ref = { first: 0, second: 7, third: 14, fourth: 21, last: -7, teenth: 12 }
     @range.find do |dy|
       dy.strftime('%A').downcase.to_sym == weekday &&
-        @range[ref[schedule]..(ref[schedule] + 6)].include?(dy)
+        @range[@ref[schedule]..(@ref[schedule] + 6)].include?(dy)
     end
   end
 end
