77c8b79fecd38c44cf5fc6ea5ae9f4e5ce14888c
Owen Tran <owen_327@hotmail.com>
2016-03-12 17:02:03 +0000

    Refactor

--- trinary/trinary.rb
+++ trinary/trinary.rb
@@ -1,8 +1,9 @@
 class Trinary
-  attr_reader :to_decimal
-
   def initialize(input)
-    @to_decimal = (/^[0-2]+$/ =~ input ? input : '0')
-                  .chars.inject(0) { |sum, num| (sum + num.to_i) * 3 } / 3
+    @value = /^[0-2]+$/ =~ input ? input : '0'
+  end
+
+  def to_decimal
+    @value.chars.inject(0) { |sum, num| (sum + num.to_i) * 3 } / 3
   end
 end
