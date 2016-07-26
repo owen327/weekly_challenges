b1244977af8e143b32f0a0bba8499c9f68d63998
Owen Tran <owen_327@hotmail.com>
2016-07-26 18:54:40 +0100

    Refector

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,13 +1,13 @@
 class Robot
   @@names = []
-  attr_accessor :name
+  attr_reader :name
 
   def initialize
     @name = reset
   end
 
   def reset
-    self.name = format('%s%s%03d', rand(65..90).chr, rand(65..90).chr, rand(1000))
+    @name = format('%s%s%03d', rand(65..90).chr, rand(65..90).chr, rand(1000))
     @@names.include?(name) ? reset : @@names << name
     name
   end
